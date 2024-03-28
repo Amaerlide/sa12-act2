a458508b56a94e833e8a7a5f27628c2656687c2f
Luis Perez <luisperez07182@email.com>
2024-03-28 01:44:45 -0500

    prob1 done

---
+++ prob1.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+
+user = 'Amaerlide'
+
+response = HTTParty.get("https://api.github.com/users/#{user}/repos")
+
+if response.success?
+  repos = response.parsed_response
+
+  highest_star = repos.max_by {|repo| repo['stargazers_count']}
+
+  puts "Name: #{highest_star["name"]}"
+  puts "Description: #{highest_star["description"]}"
+  puts "Stars: #{highest_star["stargazers_count"]}"
+  puts "Forks: #{highest_star["forks_count"]}"
+  puts "URL: #{highest_star["html_url"]}"
+
+else
+  puts "Failed to fetch repos: #{response.code}"
+end
